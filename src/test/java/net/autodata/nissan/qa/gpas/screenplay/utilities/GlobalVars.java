363452794da9da0f92b73df289ef199e0fb86f60
ankit.vaish <ankit.vaish@autodatacorp.org>
2018-03-07 19:46:18 +0530

    Added GlobalVars class to set the global variables

---
+++ src/test/java/net/autodata/nissan/qa/gpas/screenplay/utilities/GlobalVars.java
@@ -0,0 +1,11 @@
+package net.autodata.nissan.qa.gpas.screenplay.utilities;
+
+/**
+ * Created by ankit.vaish on 07-03-2018.
+ */
+public class GlobalVars {
+
+    public static String PARENT_WINDOW = "";
+    public static String ymmId = null;
+
+}
