e98104535150ed6e1ae0be59b8d877906f0e8567
ankit.vaish <ankit.vaish@autodatacorp.org>
2018-03-08 20:08:46 +0530

    Updated EditModel page object

--- src/test/java/net/autodata/nissan/qa/gpas/screenplay/ui/EditModel.java
+++ src/test/java/net/autodata/nissan/qa/gpas/screenplay/ui/EditModel.java
@@ -12,5 +12,13 @@ public class EditModel extends PageObject{
     public static final Target MODELACTION_LISTBOX = Target.the("'Action' dropdown")
             .locatedBy("//select[@class='gwt-ListBox']");
 
+    public static final Target MODEL_NAME = Target.the("Model Name")
+            .locatedBy("//tr[@class='rTool-row row_selected']/td[6]");
+
+    public static final Target INPUT_BOX_TARGET(String targetElementName , String title)
+    {
+        return  Target.the(targetElementName)
+                .locatedBy("//input[@title='"+title+"']");
+    }
 }
 
