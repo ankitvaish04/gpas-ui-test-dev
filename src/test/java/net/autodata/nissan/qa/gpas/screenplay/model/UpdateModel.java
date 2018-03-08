ce2fa32c375ff2987fd0eb5a5cc05a88e7ea3ec9
ankit.vaish <ankit.vaish@autodatacorp.org>
2018-03-08 20:06:04 +0530

    Added UpdateModel class

---
+++ src/test/java/net/autodata/nissan/qa/gpas/screenplay/model/UpdateModel.java
@@ -0,0 +1,7 @@
+package net.autodata.nissan.qa.gpas.screenplay.model;
+
+public class UpdateModel {
+    public static UpdateModelBuilder withModelInformation() {
+        return new UpdateModelBuilder();
+    }
+}
