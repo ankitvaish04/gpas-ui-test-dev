d0f6048f04e34b7f84004c85b51afe0f2147017b
ankit.vaish <ankit.vaish@autodatacorp.org>
2018-03-09 19:03:57 +0530

    - Updated UpdateModelWithRequiredValues task - Updated CreateModelWithRequiredValues task

--- src/test/java/net/autodata/nissan/qa/gpas/screenplay/tasks/UpdateModelWithRequiredValues.java
+++ src/test/java/net/autodata/nissan/qa/gpas/screenplay/tasks/UpdateModelWithRequiredValues.java
@@ -1,13 +1,15 @@
 package net.autodata.nissan.qa.gpas.screenplay.tasks;
 
-import net.autodata.nissan.qa.gpas.screenplay.customactions.DoubleClick;
-import net.autodata.nissan.qa.gpas.screenplay.customactions.RetrieveModelYear;
+import net.autodata.nissan.qa.gpas.screenplay.customactions.*;
+import net.autodata.nissan.qa.gpas.screenplay.ui.ApplicationHomePage;
 import net.autodata.nissan.qa.gpas.screenplay.ui.EditModel;
 import net.autodata.nissan.qa.gpas.screenplay.utilities.GlobalVars;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.thucydides.core.annotations.Step;
+
 
 public class UpdateModelWithRequiredValues implements Task {
 
@@ -33,12 +35,21 @@ public class UpdateModelWithRequiredValues implements Task {
     @Step("{0} looks to update a model with Effective Date as #effectivedate, Languages #languages, Currencies #currencies and Price Types #priceTypes for #ymmId and translate model information")
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
+                Enter.theValue(GlobalVars.ymmId).into(ApplicationHomePage.INPUT_BOX_TARGET("ymmID","Enter Year Make Model Id")),
+                Click.on(ApplicationHomePage.BUTTON_TARGET("Edit Button","Edit")),
+                SwitchToChildWindow.edit(),
                 RetrieveModelYear.fromHeader(),
                 DoubleClick.on(EditModel.MODEL_NAME),
                 Enter.theValue(effectivedate).into(EditModel.INPUT_BOX_TARGET("Effective Date","Effective Date")),
                 Enter.theValue(modelLineCode).into(EditModel.INPUT_BOX_TARGET("Model Line Code","Model Line Code")),
-                Enter.theValue(comment).into(EditModel.INPUT_BOX_TARGET("Comment","Comment"))
+                Enter.theValue(comment).into(EditModel.INPUT_BOX_TARGET("Comment","Comment")),
+                Click.on(EditModel.INPUT_BOX_TARGET("Languages", "Languages")),
+                SelectLanguages.byValue(languages),
+                Click.on(EditModel.BUTTON_TARGET("OK Button","OK")),
+                Click.on(EditModel.INPUT_BOX_TARGET("Currencies","Currencies")),
+                SelectCurrencies.byValue(currencies),
+                Click.on(EditModel.BUTTON_TARGET("OK Button","OK"))
 
-        );
+                );
     }
 }
