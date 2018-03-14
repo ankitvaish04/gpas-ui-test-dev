804a207c3f1c222f021417ba6e858d74e6a6c32a
ankit.vaish <ankit.vaish@autodatacorp.org>
2018-03-14 14:40:08 +0530

    Updated CreateModelWithRequiredValues task - changed modelId to modelPermId, removed retrieval of ymmId

--- src/test/java/net/autodata/nissan/qa/gpas/screenplay/tasks/CreateModelWithRequiredValues.java
+++ src/test/java/net/autodata/nissan/qa/gpas/screenplay/tasks/CreateModelWithRequiredValues.java
@@ -2,7 +2,6 @@ package net.autodata.nissan.qa.gpas.screenplay.tasks;
 
 import net.autodata.nissan.qa.gpas.screenplay.mappers.SubdivisionMapper;
 import net.autodata.nissan.qa.gpas.screenplay.ui.EditModelPage;
-import net.autodata.nissan.qa.gpas.screenplay.customactions.RetrieveYmmId;
 import net.autodata.nissan.qa.gpas.screenplay.customactions.SwitchToChildWindow;
 import net.autodata.nissan.qa.gpas.screenplay.customactions.SwitchToParentWindow;
 import net.serenitybdd.screenplay.Actor;
@@ -10,7 +9,6 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.SelectFromOptions;
-import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Step;
 
 public class CreateModelWithRequiredValues implements Task {
@@ -20,17 +18,15 @@ public class CreateModelWithRequiredValues implements Task {
     private final String subDivision;
     private final String subDivisionInInt;
     private final String modelName;
-    private final String modelId;
+    private final String modelPermId;
 
-    @Managed
-    EditModelPage editModel;
 
-    public CreateModelWithRequiredValues(String country, String year, String subDivision, String modelName, String modelId) {
+    public CreateModelWithRequiredValues(String country, String year, String subDivision, String modelName, String modelPermId) {
         this.country = country;
         this.year = year;
         this.subDivision = subDivision;
         this.modelName = modelName;
-        this.modelId = modelId;
+        this.modelPermId = modelPermId;
         this.subDivisionInInt = SubdivisionMapper.subdivisionMapper.valueOf(
                 subDivision.replaceAll("[ |/]", "_").toUpperCase()
         ).toString();
@@ -44,15 +40,15 @@ public class CreateModelWithRequiredValues implements Task {
                 Click.on(ModelDetails.CREATE_MODEL_BUTTON),
                 SelectFromOptions.byValue(year).from(ModelDetails.SELECT_MODEL_YEAR_FROM_OPTION),
                 SelectFromOptions.byValue(subDivisionInInt).from(ModelDetails.SELECT_SUBDIVISION_FROM_OPTION),
-                Enter.theValue(modelId).into(ModelDetails.CREATE_MODEL_PERM_ID),
+                Enter.theValue(modelPermId).into(ModelDetails.CREATE_MODEL_PERM_ID),
                 Enter.theValue(modelName).into(ModelDetails.CREATE_MODEL_NAME),
-                Enter.theValue(modelId).into(ModelDetails.CREATE_MODEL_SEQUENCE),
+                Enter.theValue(modelPermId).into(ModelDetails.CREATE_MODEL_SEQUENCE),
                 Click.on(ModelDetails.MODEL_SUBMIT_BUTTON),
-//              Enter.theValue(GlobalVars.ymmId).into(ApplicationHomePage.INPUT_BOX_TARGET("ymmID","Enter Year Make Model Id")),
+//              Enter.theValue(GlobalVars.ymmId).into(ApplicationHomePage.INPUT_BOX_TARGET("ymmID","Enter Year Make ModelList Id")),
 //              Click.on(ApplicationHomePage.BUTTON_TARGET("Edit Button","Edit")),
                 SwitchToChildWindow.edit(),
                 Click.on(EditModelPage.MODELACTION_LISTBOX),
-                RetrieveYmmId.fromUrl(),
+                //RetrieveYmmId.fromUrl(),
                 SelectFromOptions.byValue("Close").from(EditModelPage.MODELACTION_LISTBOX),
                 SwitchToParentWindow.parent()
 
