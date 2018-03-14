6924b66c95994ba4a35d74ec7e1195d823911200
ankit.vaish <ankit.vaish@autodatacorp.org>
2018-03-14 14:37:22 +0530

    Updated CreateModelTestSuite - corrected formatting

--- src/test/java/net/autodata/nissan/qa/gpas/screenplay/features/CreateModelTestSuite.java
+++ src/test/java/net/autodata/nissan/qa/gpas/screenplay/features/CreateModelTestSuite.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        plugin = { "pretty" },
-        features = { "classpath:features/model/create_model.feature" },
-        glue = { "classpath:net.autodata.nissan.qa.gpas.screenplay.features.steps" },
-        junit = { "--filename-compatible-names" }
+        plugin   = {"pretty"},
+        features = {"classpath:features/model/create_model.feature"},
+        glue     = {"classpath:net.autodata.nissan.qa.gpas.screenplay.features.steps"},
+        junit    = {"--filename-compatible-names"}
 )
 public class CreateModelTestSuite {}
